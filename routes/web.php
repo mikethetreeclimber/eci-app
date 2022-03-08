8426d6ab2c6e6f67b252088d442d9071ea60386e
Mike the Tree Climber <mikethetreeclimber@gmail.com>
2022-03-08 00:20:48 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Crm\Entities\Contacts;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,6 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    $contactsCount = count(Contacts::get());
+    return view('dashboard', compact('contactsCount'));
 })->name('dashboard');
