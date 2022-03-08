985ab697d71202603ebcfe7545043c62eb07f7dd
Mike the Tree Climber <mikethetreeclimber@gmail.com>
2022-03-08 00:00:56 -0500

    wip

--- resources/views/components/permission-badge.blade.php
+++ resources/views/components/permission-badge.blade.php
@@ -7,29 +7,31 @@
     class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-{{ $size }} font-medium bg-green-100 rounded-full">
     APPROVED
 </span>
-@elseif ($permissionStatus === '')
+@endif
+@if ($permissionStatus === '')
 <span
     class="flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-{{ $size }} font-medium bg-red-100 rounded-full">
     NOT APPROVED
 </span>
-@elseif ($permissionStatus === 'Refusal')
+@endif
+@if ($permissionStatus === 'Refusal')
 <span
     class="flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-{{ $size }} font-medium bg-red-100 rounded-full">
     REFUSAL
 </span>
-@elseif ($permissionStatus === 'No Contact')
+@endif
+
+@if ($permissionStatus === 'No Contact')
 <span
     class="flex-shrink-0 inline-block px-2 py-0.5 text-orange-800 text-{{ $size }} font-medium bg-orange-100 rounded-full">
     NO CONTACT
 </span>
-@elseif ($permissionStatus === null)
+@endif
+
+@if ($permissionStatus === null)
 <span
     class="flex-shrink-0 inline-block px-2 py-0.5 text-yellow-800 text-{{ $size }} font-medium bg-yellow-100 rounded-full">
     Null
 </span>
-@else
-<span
-    class="flex-shrink-0 inline-block px-2 py-0.5 text-yellow-800 text-{{ $size }} font-medium bg-yellow-100 rounded-full">
-    Unknown
-</span>
+
 @endif
